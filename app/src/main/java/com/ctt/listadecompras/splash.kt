1ec2c83a5faae9b7de1e478934d5d250c208c679
Pri Nitta <egashira.sayuri@gmail.com>
2021-03-17 22:29:16 -0300

    Splash screen add

---
+++ app/src/main/java/com/ctt/listadecompras/splash.kt
@@ -0,0 +1,26 @@
+package com.ctt.listadecompras
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+@Suppress("DEPRECATION")
+class splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
